277325999b074f1f7bd679954b7cefb3a6c36d3e
vraniic <viktor.vranic02@gmail.com>
2022-02-23 16:55:46 +0100

    nisam sve pushoovo do assd 23.02.

---
+++ section2-jsFundamentals/app.js
@@ -0,0 +1,20 @@
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('errror');
+console.clear();
+console.warn(' warning');
+console.time('Hello');
+  console.log('HI');
+  console.log('HI');
+  console.log('HI');
+  console.log('HI');
+  console.log('HI');
+  console.log('HI');
+console.timeEnd('Hello');
