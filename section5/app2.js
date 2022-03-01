27a5c9b539c5e371f90418d4c7f65f30a5ef96f0
vraniic <viktor.vranic02@gmail.com>
2022-03-01 14:42:23 +0100

    03.01.

--- section5/app2.js
+++ section5/app2.js
@@ -20,3 +20,31 @@ claculateAge() {
 const mary = new person('Mary', 'Wiliams', '11-13-1980')
 
 console.log(mary)
+
+
+// 
+// POSLEDNJA 
+// LEKCIJA
+// 
+
+class person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+
+    greeting () {
+        `hello there ${this.firstName} ${this.lastName}`
+    }
+}
+
+class customer extends person {
+    constructor(firstName,lastName, phone, membership) {
+        super(firstName, lastName)
+
+        this.phone = phone 
+        this.membership = membership
+    }
+}
+
+const john = new customer('john', 'doe', '555-555-5555', 'standard')
