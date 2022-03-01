991e1448ba76ef00be597ab908ce7d179faae545
vraniic <viktor.vranic02@gmail.com>
2022-03-01 14:30:08 +0100

    03.01.

--- section4/app.js
+++ section4/app.js
@@ -1,33 +1,112 @@
-const form = document.querySelector('#task-form')
-const taskList = document.querySelector('.collection')
-const clearBtn = document.querySelector('.clear-task')
-const filter = document.querySelector('#filter')
-const taskInput = document.querySelector('#task')
+// const form = document.querySelector('#task-form')
+// const taskList = document.querySelector('.collection')
+// const clearBtn = document.querySelector('.clear-task')
+// const filter = document.querySelector('#filter')
+// const taskInput = document.querySelector('#task')
 
-// Load all ecent listeners
-loadEventListeners()
+// // Load all ecent listeners
+// loadEventListeners()
 
-// Load all event lsitener
-function loadEventListeners () {
-    form.addEventListener('submit', addTask)
+// // Load all event lsitener
+// function loadEventListeners () {
+//     form.addEventListener('submit', addTask)
+// }
+
+// // Add task
+
+// function addTask (e) {
+//     if(taskInput.value === '') {
+//         alert('Add task')
+//     }
+
+//     // create il element
+//     const li = document .createElement('li')
+
+//     // Add class
+//     li.className
+
+//     // vreate text node and append to li
+//     li.appendChild(document.createElement)
+
+//     e.preventDefault()
+
+// }
+
+
+
+
+function calculateResults(e){
+  console.log('Calculating...');
+  
+  
+  const amount = document.getElementById('amount');
+  
+  const interest = document.getElementById('interest');
+  
+  const years = document.getElementById('years');
+  
+  const monthlyPayment = document.getElementById('monthly-payment');
+  
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal*x*calculatedInterest)/(x-1);
+
+  if(isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+  } else {
+    showError('Please check your numbers');
+  }
+
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener('submit', addTask);
 }
 
-// Add task
+// Add Task
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('Add a task');
+  }
 
-function addTask (e) {
-    if(taskInput.value === '') {
-        alert('Add task')
-    }
+  // Create li element
+  const li = document.createElement('li');
+  // Add class
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class
+  link.className = 'delete-item secondary-content';
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to li
+  li.appendChild(link);
 
-    // create il element
-    const li = document .createElement('li')
+  // Append li to ul
+  taskList.appendChild(li);
 
-    // Add class
-    li.className
+  // Clear input
+  taskInput.value = '';
 
-    // vreate text node and append to li
-    li.appendChild(document.createElement)
-
-    e.preventDefault()
-
-}
+  e.preventDefault();
+}}
--- section4/app1.js
+++ section4/app1.js
@@ -17,4 +17,60 @@ const game = document.getElementById('game'),
 
       // listen for guesses
 
-      guessBtn.addEventListener('click')
+      guessBtn.addEventListener('click', function() {
+        let guess = parseInt(guessInput.value)
+
+        // validate
+        if (isNaN(guess) || guess < min || guess > max) {
+          setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+        }
+
+        // check if won
+        if (guess === winningNum) {
+          // disabel input
+          guessInput.disabled = true
+          // change border color
+          guessInput.style.borderColor = 'green'
+        } else {
+          // wrong number 
+          guessesLeft -= 1
+          if (guessesLeft === 0) {
+            // game over - lost 
+
+            // disabble input 
+            guessInput.disabled = true
+            // change border color
+            guessInput.style.borderColor = 'red'
+            // set messsage
+            setMessage(`game over, you lost. the correct number was ${winningNum}`, 'red')
+          } else {
+            // game continues - ansqwe wrong
+
+            // change border color 
+            guessInput.style.borderColor = 'red'
+
+            // clear input 
+            guessInput.value = ''
+
+            // tell user its the wrond number 
+            setMessage(`${guess} is not correct, ${guessesLeft} guess left`, 'red')
+          }
+        }
+
+      })
+      function gameOver(won, msg) {
+        let color;
+        won === true ? color = 'green' : color = 'red'
+
+        // disable input
+        guessInput.disabled = true
+        // change border color 
+        guessInput.style.borderColor = color 
+        // set nessage 
+        setMessage(msg)
+      }
+
+      function setMessage(msg, color) {
+        message.style.color = color
+        message.textContent = msg
+      }
